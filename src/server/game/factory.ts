09f7fcdf1ea2e6a36ff5f3ba9aab55b74653e13f
woody <woodpecker146@gmail.com>
2023-10-24 11:21:03 +0700

    chore: update generic type for ServerGameFactory

--- src/server/game/factory.ts
+++ src/server/game/factory.ts
@@ -1,4 +1,9 @@
-import { ContextDecorator, serviceContainer } from '@roxavn/core/server';
+import {
+  ContextDecorator,
+  ServerModule,
+  serviceContainer,
+} from '@roxavn/core/server';
+import { BaseSocketNamespace, SocketEvents } from '@roxavn/module-socket/base';
 import {
   ServerSocketNamespace,
   makeSocketContextDecorator,
@@ -7,13 +12,18 @@ import {
 import { ServerGameManager } from './manager.js';
 import { ServerGameStorage } from './storage.js';
 
-export class ServerGameFactory extends ServerSocketNamespace {
+export class ServerGameFactory<
+  C extends SocketEvents = SocketEvents,
+  S extends SocketEvents = SocketEvents,
+  E extends SocketEvents = SocketEvents,
+  D = any,
+> extends ServerSocketNamespace<C, S, E, D> {
   static games: Record<string, { new (...args: any[]): ServerGameManager }> =
     {};
   static storages: Record<string, ServerGameStorage> = {};
   static managers: Record<string, ServerGameManager> = {};
 
-  static async getGamePresence(roomId: string) {
+  static async getGameStorage(roomId: string) {
     if (!(roomId in this.storages)) {
       const service = await serviceContainer.getAsync(ServerGameStorage);
       service.roomId = roomId;
@@ -24,18 +34,36 @@ export class ServerGameFactory extends ServerSocketNamespace {
 
   static async getGameManager(game: string, roomId: string) {
     if (!(roomId in this.managers)) {
-      const service = await serviceContainer.getAsync(this.games[game]);
-      service.gamePresence = await this.getGamePresence(roomId);
+      const service: ServerGameManager = await serviceContainer.getAsync(
+        this.games[game]
+      );
+      service.storage = await this.getGameStorage(roomId);
       this.managers[roomId] = service;
     }
     return this.managers[roomId];
   }
 
   static async closeRoom(roomId: string) {
-    const gamePresence = await ServerGameFactory.getGamePresence(roomId);
-    await gamePresence.dispose();
+    const gameStorage = await ServerGameFactory.getGameStorage(roomId);
+    await gameStorage.dispose();
     delete this.managers[roomId];
     delete this.storages[roomId];
+  }
+
+  static fromBase<
+    E extends SocketEvents = SocketEvents,
+    D = any,
+    C extends SocketEvents = SocketEvents,
+    S extends SocketEvents = SocketEvents,
+  >(
+    baseSocketNamespace: BaseSocketNamespace<C, S>,
+    serverModule: ServerModule
+  ): ServerGameFactory<C, S, E, D> {
+    return new ServerGameFactory(
+      baseSocketNamespace.name,
+      baseSocketNamespace.options,
+      serverModule
+    );
   }
 
   useGame() {
@@ -70,7 +98,7 @@ export class ServerGameFactory extends ServerSocketNamespace {
                 );
               }
             }
-            const service = await ServerGameFactory.getGamePresence(roomId);
+            const service = await ServerGameFactory.getGameStorage(roomId);
             return service;
           }
           throw new Error(
