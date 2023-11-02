62b951b3c14b9ffc47098420b1726ad8b92365ea
woody <woodpecker146@gmail.com>
2023-11-02 13:56:07 +0700

    feat: add params to changeScene

--- src/web/components/GameSceneManager.tsx
+++ src/web/components/GameSceneManager.tsx
@@ -3,27 +3,36 @@ import { createContext, createElement, useContext, useState } from 'react';
 import { GameScene, GameSceneProps } from './GameScene.js';
 
 export function createSceneManager<
-  T extends Record<string, React.ComponentType>,
+  T extends Record<string, React.ComponentType<any>>,
 >(scenes: T) {
   const sceneContext = createContext<{
     scene: keyof T;
-    changeScene: (scene: keyof T) => void;
+    changeScene: <K extends keyof T>(
+      scene: K,
+      params?: T[K] extends React.ComponentType<infer P> ? P : never
+    ) => void;
     setDomChildren: (domChildren: React.ReactElement[]) => void;
   }>({} as any);
 
   return {
     useState: () => useContext(sceneContext),
     Provider: (props: Omit<GameSceneProps, 'domChildren' | 'children'>) => {
-      const [scene, changeScene] = useState<keyof T>(Object.keys(scenes)[0]);
+      const [sceneData, setSceneData] = useState<{ name: any; params?: any }>({
+        name: Object.keys(scenes)[0],
+      });
       const [domChildren, setDomChildren] = useState<React.ReactElement[]>();
 
       return (
         <GameScene {...props} domChildren={domChildren}>
           <AuthProvider>
             <sceneContext.Provider
-              value={{ scene, changeScene, setDomChildren }}
+              value={{
+                scene: sceneData.name,
+                changeScene: (name, params) => setSceneData({ name, params }),
+                setDomChildren,
+              }}
             >
-              {createElement(scenes[scene])}
+              {createElement(scenes[sceneData.name], sceneData.params)}
             </sceneContext.Provider>
           </AuthProvider>
         </GameScene>
